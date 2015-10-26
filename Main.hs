13c02d6e749c4b9a0038f1f531af05adb6151dba
vstonkus <visvaldas.stonkus@gmail.com>
2015-10-26 22:29:24 +0200

    Files names changed

--- Main.hs
+++ Main.hs
@@ -1,5 +1,4 @@
-module Lecture2 
-where
+module Main where
 
 import Data.List
 import Data.List.Utils
@@ -7,21 +6,6 @@ import Data.List.Utils
 --------------(  V ,  X ,  Y )
 type Action = (Char, Int, Int)
 type Board = [Action]
-
-msg1  = "le"
-msg2  = "ld1:v1:x1:xi2e1:yi0eee"
-msg3  = "ld1:v1:x1:xi0e1:yi2eed1:v1:o1:xi1e1:yi0eed1:v1:x1:xi0e1:yi0eee"
-msg4  = "ld1:v1:x1:xi2e1:yi0eed1:v1:o1:xi0e1:yi2eed1:v1:x1:xi1e1:yi1eee"
-msg5  = "ld1:v1:x1:xi2e1:yi2eed1:v1:o1:xi1e1:yi1eed1:v1:x1:xi0e1:yi0eee"
-msg6  = "ld1:v1:x1:xi0e1:yi0eed1:v1:o1:xi0e1:yi1eed1:v1:x1:xi2e1:yi1eed1:v1:o1:xi2e1:yi0eed1:v1:x1:xi2e1:yi2eee"
-msg7  = "ld1:v1:x1:xi2e1:yi2eed1:v1:o1:xi0e1:yi2eed1:v1:x1:xi2e1:yi1eed1:v1:o1:xi1e1:yi2eed1:v1:x1:xi2e1:yi0eee"
-msg8  = "ld1:v1:x1:xi0e1:yi2eed1:v1:o1:xi1e1:yi0eed1:v1:x1:xi2e1:yi1eed1:v1:o1:xi0e1:yi0eed1:v1:x1:xi1e1:yi1eed1:v1:o1:xi2e1:yi0eee"
-msg9  = "ld1:v1:x1:xi0e1:yi0eed1:v1:o1:xi0e1:yi1eed1:v1:x1:xi2e1:yi2eed1:v1:o1:xi1e1:yi0eed1:v1:x1:xi1e1:yi2eed1:v1:o1:xi1e1:yi1eed1:v1:x1:xi0e1:yi2eee"
-msg10 = "ld1:v1:x1:xi0e1:yi1eed1:v1:o1:xi1e1:yi2eed1:v1:x1:xi1e1:yi0eed1:v1:o1:xi2e1:yi1eed1:v1:x1:xi0e1:yi2eed1:v1:o1:xi2e1:yi0eed1:v1:x1:xi0e1:yi0eee"
-msg11 = "ld1:v1:x1:xi2e1:yi2eed1:v1:o1:xi1e1:yi0eed1:v1:x1:xi2e1:yi0eed1:v1:o1:xi0e1:yi0eed1:v1:x1:xi0e1:yi2eed1:v1:o1:xi1e1:yi1eed1:v1:x1:xi2e1:yi1eee"
-msg12 = "ld1:v1:x1:xi0e1:yi1eed1:v1:o1:xi2e1:yi2eed1:v1:x1:xi1e1:yi2eed1:v1:o1:xi2e1:yi1eed1:v1:x1:xi0e1:yi0eed1:v1:o1:xi1e1:yi1eed1:v1:x1:xi1e1:yi0eed1:v1:o1:xi2e1:yi0eee"
-msg13 = "ld1:v1:x1:xi2e1:yi0eed1:v1:o1:xi1e1:yi1eed1:v1:x1:xi1e1:yi2eed1:v1:o1:xi2e1:yi1eed1:v1:x1:xi0e1:yi2eed1:v1:o1:xi2e1:yi2eed1:v1:x1:xi0e1:yi0eed1:v1:o1:xi0e1:yi1eee"
-msg14 = "ld1:v1:x1:xi2e1:yi2eed1:v1:o1:xi1e1:yi2eed1:v1:x1:xi2e1:yi1eed1:v1:o1:xi0e1:yi0eed1:v1:x1:xi0e1:yi1eed1:v1:o1:xi1e1:yi1eed1:v1:x1:xi1e1:yi0eed1:v1:o1:xi2e1:yi0eed1:v1:x1:xi0e1:yi2eee"
 
 decode :: String -> Board
 decode ('l' : msg) = 
